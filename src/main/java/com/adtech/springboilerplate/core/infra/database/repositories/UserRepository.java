94c4c18ea640d7220d6efcff43611ff8ed5e3887
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2026-05-21 21:48:34 -0300

    refactor: update UserRepository methods for improved type safety and consistency

--- src/main/java/com/adtech/springboilerplate/core/infra/database/repositories/UserRepository.java
+++ src/main/java/com/adtech/springboilerplate/core/infra/database/repositories/UserRepository.java
@@ -3,8 +3,11 @@ package com.adtech.springboilerplate.core.infra.database.repositories;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Function;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 import org.springframework.stereotype.Repository;
 
 import com.adtech.springboilerplate.core.infra.database.models.UsersModel;
@@ -12,7 +15,7 @@ import com.adtech.springboilerplate.core.infra.database.models.UsersModel;
 @Repository
 public interface UserRepository extends JpaRepository<UsersModel, UUID> {
 	@SuppressWarnings("null")
-	<UM extends UsersModel> UM save(UM entity);
+	<S extends UsersModel> S save(S entity);
 
 	@SuppressWarnings("null")
 	Optional<UsersModel> findById(UUID id);
@@ -20,10 +23,22 @@ public interface UserRepository extends JpaRepository<UsersModel, UUID> {
 	UsersModel findByFullName(String fullName);
 
 	@SuppressWarnings("null")
+	<S extends UsersModel, R> R findBy(Example<S> example, Function<FetchableFluentQuery<S>, R> queryFunction);
+
+	@SuppressWarnings("null")
+	<S extends UsersModel> Optional<S> findOne(Example<S> example);
+
+	@SuppressWarnings("null")
 	LinkedList<UsersModel> findAll();
 
 	long count();
 
 	@SuppressWarnings("null")
+	<S extends UsersModel> long count(Example<S> example);
+
+	@SuppressWarnings("null")
 	void delete(UsersModel entity);
+
+	@SuppressWarnings("null")
+	void deleteById(UUID id);
 }
